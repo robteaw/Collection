88ea9237bdc3dd7c7bf100da7c8b2e19ba0d4b4d
Robert Tea <robteaw@gmail.com>
2022-09-03 23:59:48 -0500

    4. Contact Form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Form from "./Form"
 
 export default function Contact() {
   return (
     <Container>
-      <p>Contact</p>
+      <h1>Contact</h1>
+      <p>If you have any questions or suggestions, feel free to leave a message. Also if there are any movie(s) you would like to see, choose a movie from the list in the dropdown.</p>
+      <Form />
     </Container>
   );
 }
@@ -13,4 +16,7 @@ export default function Contact() {
 const Container = styled.div`
   height: 100vh;
   padding: 8rem 15%;
+  p {
+    margin-bottom: 1.5rem;
+  }
 `;
---
+++ src/pages/Contact/Form.js
@@ -0,0 +1,218 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+export default function Form() {
+  // Format phone number
+  const [inputValue, setInputValue] = useState("");
+  const handleInput = (e) => {
+    const formattedPhone = formatPhone(e.target.value);
+    setInputValue(formattedPhone);
+  };
+
+  function formatPhone(value) {
+    if (!value) return value;
+    const phoneNumber = value.replace(/[^\d]/g, "");
+    const phoneNumberLength = phoneNumber.length + 1;
+    if (phoneNumberLength < 4) return phoneNumber;
+    if (phoneNumberLength < 7) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    }
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+      3,
+      6
+    )}-${phoneNumber.slice(6, 10)}`;
+  }
+
+  // Form submit stays on same page
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // Submit message
+  const [message, setMessage] = useState(false);
+  const showMessage = () => setMessage(!message);
+  return (
+    <Container>
+      {" "}
+      <form action="/contact" name="contact" method="POST" onSubmit={onSubmit}>
+        <input type="hidden" name="form-name" value="contact" />
+        <div>
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="First Name"
+            class="form-element"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Last Name"
+            class="form-element"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            className="form-element"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="tel"
+            name="phoneNumber"
+            id="phoneNumber"
+            placeholder="Phone Number"
+            class="form-element"
+            required
+            onChange={(e) => handleInput(e)}
+            value={inputValue}
+          />
+        </div>
+        <Reason>
+          <select name="reason" id="reason">
+            <option disabled selected>
+              Movie
+            </option>
+            <option value="1">#</option>
+            <option value="2">#</option>
+            <option value="3">#</option>
+          </select>
+        </Reason>
+        <div class="form-text">
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            class="form-element"
+            required
+          ></textarea>
+        </div>
+        <Submit>
+          {message ? (
+            <span className="message">
+              <h2>Form Submitted!</h2>
+            </span>
+          ) : (
+            <input type="submit" value="SEND" onClick={showMessage} />
+          )}
+        </Submit>
+      </form>
+    </Container>
+  );
+}
+
+// styling
+const Container = styled.div`
+  form {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 16px;
+    align-items: center;
+    justify-content: center;
+    @media (min-width: 768px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    .form-element {
+      grid-column: 1 / -1;
+      appearance: none;
+      outline: none;
+      border: none;
+      display: block;
+      width: 100%;
+      border-radius: 8px;
+      padding: 12px 16px;
+      background-color: #f3f3f3;
+      transition: 0.4s;
+      &:focus {
+        box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.2);
+      }
+    }
+    input,
+    select {
+      width: 100%;
+      margin: 0.5rem 0;
+      padding: 0.75rem;
+      border-radius: 0.5rem;
+    }
+    input:focus {
+      background-color: var(--subText);
+    }
+    textarea:focus {
+      background-color: var(--subText);
+    }
+    .form-text {
+      grid-column: 1 / -1;
+    }
+    textarea {
+      resize: vertical;
+      min-height: 100px;
+      padding: 0.75rem;
+    }
+  }
+`;
+
+const Reason = styled.div`
+  select {
+    appearance: none;
+    border: 0;
+    font: inherit;
+    width: 100%;
+    color: var(--subColor);
+    background: url(https://upload.wikimedia.org/wikipedia/commons/9/9d/Caret_down_font_awesome_whitevariation.svg)
+        no-repeat right 0.8em center / 1.4em,
+      linear-gradient(to left, var(--selectColor) 3em, var(--btnColor) 3em);
+    color: black;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+      0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    cursor: pointer;
+    option {
+      color: inherit;
+      background-color: var(--textHover);
+    }
+    /* Remove focus outline */
+    &:focus {
+      outline: none;
+    }
+    /* Remove IE arrow */
+    &::-ms-expand {
+      display: none;
+    }
+  }
+`;
+
+const Submit = styled.div`
+  grid-column: 1 / -1;
+  input[type="submit"] {
+    color: var(--btnColor);
+    background: transparent;
+    font-size: 1rem;
+    font-weight: 600;
+    width: 10rem;
+    padding: 0.7rem;
+    margin-top: 1rem;
+    border: 2px solid var(--btnColor);
+    border-radius: 5rem;
+    cursor: pointer;
+    transition: 0.4s;
+    &:hover {
+      color: var(--btnColor2);
+      background: var(--btnHover);
+      border: 2px solid transparent;
+    }
+  }
+  .message {
+    color: var(--messageColor);
+    font-weight: bold;
+    transition: 0.4s;
+  }
+`;
